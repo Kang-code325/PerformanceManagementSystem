41bb0a1d2c75f5da9e5fa66a9d4dd233d1bf8697
Kang-code325 <657771020@qq.com>
2024-10-11 21:18:53 +0800

    解决中文乱码问题

--- src/com/wk/system/web/servlet/EmployeeServlet.java
+++ src/com/wk/system/web/servlet/EmployeeServlet.java
@@ -21,6 +21,8 @@ public class EmployeeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
         String action = request.getParameter("action");
 
         System.out.println("action"+action);
@@ -33,6 +35,8 @@ public class EmployeeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
         String action = request.getParameter("action");
 
         System.out.println("action"+action);
